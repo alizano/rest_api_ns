880aa6146c620c2ebd9d7a3f656950e27bd5409d
Arturo Lizano Vargas <arturolizano@gmail.com>
2019-06-21 20:43:27 -0600

    Spring Boot Init

---
+++ src/test/java/com/alizano/restapins/RestApiNsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alizano.restapins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApiNsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
